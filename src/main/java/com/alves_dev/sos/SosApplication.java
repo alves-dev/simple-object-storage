a7f26fd7f58482c8450267049e30acd537d4ec0f
alves-dev <alvesmoreiraigor@email.com>
2026-02-17 06:44:38 -0300

    init project

---
+++ src/main/java/com/alves_dev/sos/SosApplication.java
@@ -0,0 +1,13 @@
+package com.alves_dev.sos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alves_dev/sos/SosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves_dev.sos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
